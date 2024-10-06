894b9eb509d007094ddacd65fc1d464e6d6f7eba
developerTZDA <>
2024-10-06 13:26:52 +0800

    init

---
+++ src/view/home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="home-page">小六壬</div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const data = ref(0)
+console.log(data)
+</script>
+
+<style scoped lang="less">
+.home-page {
+	min-height: 100vh;
+}
+</style>
